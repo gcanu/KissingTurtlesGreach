46f3d9a4bbd17288dfe52bb350a5e2dd43c5450d
corinnekrych <corinnekrych@gmail.com>
2013-01-25 16:08:53 +0100

    restore

---
+++ ktDrawBackground.js
@@ -0,0 +1,25 @@
+/**
+ * Function that draws the background.
+ *
+ * Help:
+ *
+ * canvas.getContext('2d')
+ * ctx.beginPath()
+ * ctx.fillStyle/strokeStyle = 'red' | 'rgb(250, 30, 120)' | 'rgba(250, 30, 120, 0.5)'
+ * ctx.lineWidth = 3
+ * ctx.moveTo(x, y)
+ * ctx.lineTo(x, y)
+ * ctx.closePath()
+ * ctx.fill()
+ * ctx.stroke()
+ * ctx.rect(x, y, w, h)
+ * ctx.arc(x, y, r, startangle, endangle)
+ *
+ * @param canvas the canvas to draw to.
+ * @param grid the size of the grid.
+ **/
+window.ktDrawBackground = (function() {
+  return function(canvas, grid) {
+    // TODO
+  };
+})();
